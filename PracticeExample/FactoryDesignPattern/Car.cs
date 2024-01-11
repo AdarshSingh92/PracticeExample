db27f31ec69b28e781d6165b501eddef48b1ff76
AdarshSingh92 <singhadarsh271@gmail.com>
2024-01-11 17:08:14 +0530

    added sample code for factory design pattern

---
+++ PracticeExample/FactoryDesignPattern/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class Car : ICar
+    {
+        public void DriveCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCardDetails()
+        {
+            Console.WriteLine("This is common car");
+        }
+    }
+}
---
+++ PracticeExample/FactoryDesignPattern/SUVCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class SUVCar : ICar
+    {
+        public void DriveCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCardDetails()
+        {
+            Console.WriteLine("This is Nexon SUV car");
+        }
+    }
+}
