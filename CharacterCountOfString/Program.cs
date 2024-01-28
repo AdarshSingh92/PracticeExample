0044a37e6cced0e87dc901391036d33a7f00b7df
AdarshSingh92 <singhadarsh271@gmail.com>
2024-01-28 10:14:16 +0530

    added some sample project

---
+++ CharacterCountOfString/Program.cs
@@ -0,0 +1,10 @@
+﻿IList<int> myInts = new List<int>();
+myInts.Add(1);
+myInts.Add(2);
+myInts.Add(3);
+myInts.Add(4);
+myInts.Add(5);
+myInts.Add(6);
+
+var obj = myInts.Select(x=> (x* 2).ToString());
+
